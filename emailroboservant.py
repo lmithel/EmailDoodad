c639e3e9fba4ee54a70cfe7eb4d596f91241a08a
Lexi Mithel <Lexi@hollys-ipad.bacl.org>
2015-07-24 15:07:34 -0700

    changed naming conventions

--- emailroboservant.py
+++ emailroboservant.py
@@ -1,15 +1,16 @@
 import poplib
 import os
 import email
+import datetime
 import getpass
 from email import parser
 
-user = raw_input("Username: ")
-pw = getpass.getpass()
+#user = raw_input("Username: ")
+#pw = getpass.getpass()
 
 Mailbox = poplib.POP3_SSL('pop.gmail.com',995)
-Mailbox.user(user)
-Mailbox.pass_(pw) 
+Mailbox.user('recent:email.filer@gobaci.com')
+Mailbox.pass_('Filesfordays') 
 
 Mailbox.set_debuglevel(0)
 #Get messages from server:
@@ -17,14 +18,27 @@ numMessages = len(Mailbox.list()[1])
 messages = [Mailbox.retr(i) for i in range(1, len(Mailbox.list()[1]) + 1)]
 # Concat message pieces:
 messages = ["\n".join(mssg[1]) for mssg in messages]
-#Parse message intom an email object:
+#Parse message into an email object:
 messages = [parser.Parser().parsestr(mssg) for mssg in messages]
-allowed_mimetypes = ["application/vnd.openxmlformats-officedocument.wordprocessingml.document","image/jpeg","application/pdf","image/png","application/vnd.ms-excel",]
+
+allowed_mimetypes = ["application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+"application/vnd.openxmlformats-officedocument.wordprocessingml.template","application/msword"
+"application/vnd.ms-word.document.macroenabled.12","application/vnd.ms-word.template.macroenabled.12",
+"image/jpeg","application/pdf","image/png","image/tiff","application/vnd.ms-excel","application/zip",
+"application/x-7z-compressed","application/vnd.ms-excel","application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"]
+
 for message in messages:
 	if message['subject'] == '':
 		path = 'misc'
 	else:
-		path = message['subject']
+		subject = message['subject'].split(':');
+		if subject[0] == 'Fwd' or subject[0] == 'Re':
+			subject.pop(0) #Remove the Fwd or Re
+			subject[0] = subject[0][1:] #Remove the first space from the name after the colon
+		filename = subject[0] #Filename is the name of the individual + What It's Regarding
+		path = subject[0].split('-')[0] #Directory is the name of the individual
+
+
 	date = message['Date']
 	#time = message['Received']
 	who = message['From']
@@ -40,20 +54,18 @@ for message in messages:
 			attachment_name = part.get_filename()
 			data = part.get_payload(decode=True)
 			if os.path.isfile(path + "/" + attachment_name):
-				os.mkdir(path + "/" + who)
-				f = open(path + "/" + who + "/" + attachment_name,'wb')
-				print f
+				now = str(datetime.datetime.now())
+				f = open(path + "/" + now + " " + attachment_name,'wb')
 			else:
 				f = open(path + "/" + attachment_name, 'wb')
 			f.write(data)
 			f.close()
 
 	if os.path.isfile(path + "/" + date + " " + who):
-		text_file = open(path + "/" + date + " " + who + messageid, "w")
+		text_file = open(path + "/" + filename + messageid, "w")
 	else:
-		text_file = open(path + "/" + date + " " + who, "w")
+		text_file = open(path + "/" + filename, "w")
 	text_file.write(msg)
 	text_file.close()
-
 
 Mailbox.quit()
